5edf881702f03e2085d8aae0de7d292d13a73abf
khaleel.dev <kkhaleel.dev@gmail.com>
2026-02-08 00:14:57 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,17 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-// import { AuthProvider } from "./context/AuthContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <AuthProvider>
-//       <App />
-//     </AuthProvider>
-//   </BrowserRouter>
-// );
