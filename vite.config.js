7ab03c29b29f0e457f027c8a0e20e39f0b5c68cc
khaleel.dev <kkhaleel.dev@gmail.com>
2026-02-08 10:15:44 +0530

    vite old

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
+  base: "/", // use root base
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 });
