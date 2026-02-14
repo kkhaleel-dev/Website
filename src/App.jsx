1e9a9d084b4943d7216d92118333b901d685227a
khaleel.dev <kkhaleel.dev@gmail.com>
2026-02-14 19:28:16 +0530

    V1.0.3 - routing issue fix & vercel config removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   useLocation,
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -75,7 +75,7 @@ const Footer = () => {
         </a>
       </p>
 
-      <p className="version">Version 1.0.2</p>
+      <p className="version">Version 1.0.3</p>
     </footer>
   );
 };
